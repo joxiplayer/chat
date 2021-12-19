dc6225d67e35d66953f2a59b67e946fa35efe34c
joxiplayer <joxiplayer.id@gmail.com>
2021-12-19 11:50:30 +0700

    first commit

--- data/config/config.js
+++ data/config/config.js
@@ -1,11 +1,10 @@
 const firebaseConfig = {
     apiKey: "AIzaSyAdE6ZPQGCpZL2YHJEqZcKkyxHqnBM7EI8",
-  authDomain: "joxiplayer-data.firebaseapp.com",
-  databaseURL: "https://joxiplayer-data-default-rtdb.firebaseio.com",
-  projectId: "joxiplayer-data",
-  storageBucket: "joxiplayer-data.appspot.com",
-  messagingSenderId: "1057864895288",
-  appId: "1:1057864895288:web:c5204c68c2570d1b2fcfcd",
-  measurementId: "G-W2XZX441RV"
+    authDomain: "joxiplayer-data.firebaseapp.com",
+    databaseURL: "https://joxiplayer-data-default-rtdb.firebaseio.com",
+    projectId: "joxiplayer-data",
+    storageBucket: "joxiplayer-data.appspot.com",
+    messagingSenderId: "1057864895288",
+    appId: "1:1057864895288:web:c5204c68c2570d1b2fcfcd",
 };
 const app = firebase.initializeApp(firebaseConfig);
